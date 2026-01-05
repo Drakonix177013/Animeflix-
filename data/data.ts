e193dbd31eb0e79c37d63a0a4f155d8f66dff8b2
Drakonix <rgupta051983@gmail.com>
2026-01-05 21:42:47 +0530

    Add files via upload

---
+++ data/data.ts
@@ -0,0 +1,31 @@
+
+export const animeList = [
+  {
+    id: "1",
+    title: "Naruto",
+    poster: "https://i.imgur.com/UYiroys.jpg",
+    banner: "https://i.imgur.com/8Km9tLL.jpg",
+    seasons: [
+      {
+        season: 1,
+        episodes: [
+          {
+            id: "e1",
+            title: "Episode 1",
+            video: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export const movieList = [
+  {
+    id: "m1",
+    title: "Your Name",
+    poster: "https://i.imgur.com/QCNbOAo.jpg",
+    banner: "https://i.imgur.com/UPrs1EW.jpg",
+    video: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+  },
+];
